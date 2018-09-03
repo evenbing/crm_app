fc24e561443d65f208d0ca45c970e8eb0df496d2
kanbuting <1297501338@qq.com>
2018-09-03 19:11:46 +0800

    使用 FlatList

--- src/pages/Modal/SelectYear.js
+++ src/pages/Modal/SelectYear.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
 import uuidv1 from 'uuid/v1';
-import { Content, List, ListItem, Left, Right, Icon, Text } from 'native-base';
+import { ListItem, Left, Right, Icon, Text } from 'native-base';
 import { CommStatusBar, LeftBackIcon } from '../../components/Layout';
 import { theme } from '../../constants';
 
@@ -10,6 +9,8 @@ const ContainerView = styled.View`
   flex: 1;
   background-color: ${theme.whiteColor};
 `;
+
+const FlatList = styled.FlatList``;
 
 class SelectYear extends Component {
   constructor(props) {
@@ -27,6 +28,21 @@ class SelectYear extends Component {
     navigation.goBack();
   }
 
+  onScrollToIndex = () => {
+    const randomIndex = Math.floor(Math.random() * this.state.years.length);
+    this.flatListRef.scrollToIndex({
+      animated: true,
+      index: randomIndex,
+      viewPosition: 0.3,
+    });
+  }
+
+  getItemLayout = index => ({
+    length: 50.5,
+    offset: 50.5 * index,
+    index,
+  })
+
   generateYears = () => {
     const ys = [];
     const { selectedYear } = this.props.navigation.state.params;
@@ -40,30 +56,54 @@ class SelectYear extends Component {
     return ys;
   }
 
+  generateKeyExtractor = item => item.key;
+
+  renderItem = ({ item }) => {
+    const {
+      value,
+      selected,
+    } = item;
+
+    return (
+      <ListItem
+        selected={selected}
+        onPress={this.onPress(item)}
+      >
+        <Left>
+          <Text> {value} </Text>
+        </Left>
+        <Right>
+          <Icon name="arrow-forward" />
+        </Right>
+      </ListItem>
+    );
+  }
+
   render() {
     return (
       <ContainerView>
         <CommStatusBar />
-        <Content>
-          <List>
-            {
-              this.state.years.map(item => (
-                <ListItem
-                  key={item.key}
-                  selected={item.selected}
-                  onPress={this.onPress(item)}
-                >
-                  <Left>
-                    <Text> {item.value} </Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-              ))
-            }
-          </List>
-        </Content>
+        {/* <TouchableOpacity
+          style={{
+            padding: 10,
+            borderColor: 'red',
+            borderWidth: 2,
+            alignItems: 'center',
+          }}
+          onPress={this.onScrollToIndex}
+        >
+          <Text>123</Text>
+        </TouchableOpacity> */}
+        <FlatList
+          ref={(ref) => { this.flatListRef = ref; }}
+          data={this.state.years}
+          getItemLayout={(data, index) => {
+            return { length: 50.5, index, offset: 50.5 * index };
+          }}
+          keyExtractor={this.generateKeyExtractor}
+          renderItem={this.renderItem}
+          // ItemSeparatorComponent={this.renderItemSeparatorComponent}
+        />
       </ContainerView>
     );
   }
